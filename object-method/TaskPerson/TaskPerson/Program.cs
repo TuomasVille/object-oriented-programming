f869357a83470d22592cf72adb19ec7443a2c2e3
TuomasVille <tuomas_kayhty@hotmail.com>
2018-01-12 13:40:02 +0200

    TaskPerson 1.0

--- object-method/TaskPerson/TaskPerson/Program.cs
+++ object-method/TaskPerson/TaskPerson/Program.cs
@@ -8,8 +8,11 @@ namespace TaskPerson
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Person personTuomas = new Person();
+            Console.WriteLine($"Tuomaksen ikä: {personTuomas.GetAge()}");
+            
             personTuomas.Name = "Tuomas";
             personTuomas.GrowOld();
+            personTuomas.GrowOld(20);
             personTuomas.PrintPersonInfo();
 
             Person newPerson = new Person("Joe Doe", 17);
