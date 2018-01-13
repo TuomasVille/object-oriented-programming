0ba8efb4233571ec2fbdb585ef22568e4b334b4a
TuomasVille <tuomas_kayhty@hotmail.com>
2018-01-13 14:43:02 +0200

    TaskCompany 1.0

---
+++ object-method/TaskCompany/TaskCompany/ClassCompany.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCompany
+{
+    class ClassCompany
+    {
+        //Fields
+        public string Title;
+        public string Address;
+        public int Phone;
+        private double _outcome;
+        private double _expense;
+
+        //Constructor
+        public ClassCompany()
+        {
+            Title = "Unknown";
+            Address = "Unknown";
+            Phone = 0;
+            _outcome = 0;
+            _expense = 0;
+        }
+
+        public ClassCompany(string title, string address, int phone, double outcome, double expense)
+        {
+            Title = title;
+            Address = address;
+            this.Phone = phone;
+            this._outcome = outcome;
+            this._expense = expense;
+        }
+        
+        //Copy constructor
+        public ClassCompany(ClassCompany previousClassCompany)
+        {
+            Title = previousClassCompany.Title;
+            Address = previousClassCompany.Address;
+            this.Phone = previousClassCompany.Phone;
+            this._outcome = previousClassCompany._outcome;
+            this._expense = previousClassCompany._expense;
+        }
+
+        //Methods
+        public void AskData()
+        {
+            Console.WriteLine("Syötä yrityksen nimi: ");
+            Title = Console.ReadLine();
+
+            Console.WriteLine("Syötä osoite: ");
+            Address = Console.ReadLine();
+
+            Console.WriteLine("Syötä puhelinnumero: ");
+            Phone = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Syötä tulot: ");
+            _outcome = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Syötä menot: ");
+            _expense = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowCompanyInfo()
+        {
+            Console.WriteLine($"Yrityksen nimi: {Title}\n" +
+                              $"Osoite: {Address}\n" +
+                              $"Puh. {Phone.ToString($"0## #### ###")}\n" +
+                              $"Tulot: {_outcome:C}\n" +
+                              $"Menot: {_expense:C}\n");
+        }
+
+        public void Profit(ClassCompany previousClassCompany)
+        {
+            double profit = (previousClassCompany._outcome - previousClassCompany._expense)/ previousClassCompany._expense * 100;
+
+            if(profit < 100 )
+            {
+                Console.WriteLine($"Yrityksellä {previousClassCompany.Title} menee heikosti. Voitto on alle 100% suurempi kuin menot!");
+            }
+            else if(profit >= 100 && profit <= 300)
+            {
+                Console.WriteLine($"Yrityksellä {previousClassCompany.Title} menee tyydyttävästi. Voitto on 100% tai yli, mutta alle 300% suurempi kuin menot!");
+            }
+            else
+            {
+                Console.WriteLine($"Yrityksellä {previousClassCompany.Title} menee loistavasti. Voitto on yli 300% suurempi kuin menot!");
+            }
+        }      
+    }
+}
---
+++ object-method/TaskCompany/TaskCompany/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TaskCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            ClassCompany company = new ClassCompany();
+            company.AskData();
+            Console.WriteLine();
+            company.ShowCompanyInfo();
+            company.Profit(company);
+            Console.WriteLine();
+
+            ClassCompany company2 = new ClassCompany();
+            company2.AskData();
+            Console.WriteLine();
+            company2.ShowCompanyInfo();
+            company2.Profit(company2);
+            Console.WriteLine();
+
+            ClassCompany company3 = new ClassCompany();
+            company3.AskData();
+            Console.WriteLine();
+            company3.ShowCompanyInfo();
+            company3.Profit(company3);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
