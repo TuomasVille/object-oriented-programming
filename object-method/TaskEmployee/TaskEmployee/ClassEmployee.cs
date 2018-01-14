47e380ce36494956912526502113459d922fc423
TuomasVille <tuomas_kayhty@hotmail.com>
2018-01-14 13:53:51 +0200

    TaskEmployee 2.0

---
+++ object-method/TaskEmployee/TaskEmployee/ClassEmployee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskEmployee
+{
+    class ClassEmployee
+    {
+        //Fields
+        public string Name;
+        public int Id;
+        public string Position;
+        public double Salary;
+        public static double NewSalary = 0;
+
+        //Constructor
+        public ClassEmployee ()
+        {
+            Name = "Unknown";
+            Id = 0;
+            Position = "Unknown";
+            Salary = 0;
+        }
+
+        public ClassEmployee(string name, int id, string position, double salary)
+        {
+            Name = name;
+            this.Id = id;
+            Position = position;
+            this.Salary = salary;
+        }
+
+        //Methods
+        public void PrintEmployeeInfo(ClassEmployee employee)
+        {
+            if(employee.Id == 1401700)
+            {
+                NewSalary = employee.Salary;
+            }
+
+            Console.WriteLine($"Name: {employee.Name}\n" +
+                              $"Id: {employee.Id}\n" +
+                              $"Position: {employee.Position}\n" +
+                              $"Salary: {employee.Salary:C}\n" +
+                              $"Compared to CEO´s salary: {CompareSalary(employee):C}");
+                              
+        }
+
+        public static double CompareSalary(ClassEmployee employee)
+        {
+            double Comparison = 0;
+
+            Comparison = employee.Salary - NewSalary;
+
+            return Comparison;  
+        }       
+    }
+}
---
+++ object-method/TaskEmployee/TaskEmployee/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            ClassEmployee[] employees = new ClassEmployee[3];
+            employees[0] = new ClassEmployee("Kelly", 1401700, "CEO", 250000);
+            employees[0].PrintEmployeeInfo(employees[0]);
+            Console.WriteLine();
+
+            employees[1] = new ClassEmployee("Olly", 1401701, "Marketing manager", 35000);
+            employees[1].PrintEmployeeInfo(employees[1]);
+            Console.WriteLine();
+
+            employees[2] = new ClassEmployee("James", 1401702, "Marketing", 25000);
+            employees[2].PrintEmployeeInfo(employees[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
