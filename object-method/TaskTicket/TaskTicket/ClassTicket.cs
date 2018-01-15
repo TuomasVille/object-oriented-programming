542f28273f1ac662a9df905eddf6dbb1af2de997
TuomasVille <tuomas_kayhty@hotmail.com>
2018-01-15 15:00:26 +0200

    TaskTicket 1.0

---
+++ object-method/TaskTicket/TaskTicket/ClassTicket.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTicket
+{
+    class ClassTicket
+    {
+        //Fields
+        public int Age;
+        public int Price;
+        public float Discount;
+
+        //Constructor
+        public ClassTicket()
+        {
+            Age = 0;
+            Price = 16;
+            Discount = 0.0f;
+        }
+
+        public ClassTicket(int age, int price, float discount)
+        {
+            this.Age = age;
+            this.Price = price;
+            this.Discount = discount;           
+        }
+
+        //Methods
+        public void AskData()
+        {
+            Console.Write("Age: ");
+            Age = int.Parse(Console.ReadLine());       
+        }
+
+        public void ShowPrice(ClassTicket ticket)
+        {
+            if (Age < 7)
+            {
+                Console.WriteLine("Price = " + Price * 0 + "€");
+            }
+            else if (Age >= 7 && Age <= 15)
+            {
+                Console.WriteLine("Price = " + Price * 0.5 + "€");
+            }
+            else if (Age >= 65)
+            {
+                Console.WriteLine("Price = " + Price * 0.5 + "€");
+            }
+            if (Age > 15 && Age < 65)
+            {
+                Console.Write("Soldier? [Y/N]: ");
+                if (Console.ReadLine().ToUpper() == "Y")
+                    Console.WriteLine("Price = " + Price * 0.5 + "€");
+
+                else
+                {
+                    Console.Write("Mtk member? [Y/N]: ");
+                    if (Console.ReadLine().ToUpper() == "Y")
+                    {
+                        Discount = 0.15f;
+                    }
+                    Console.Write("Student? [Y/N]: ");
+                    if (Console.ReadLine().ToUpper() == "Y")
+                    {
+                        Discount = Discount + 0.45f;
+                    }
+                    Console.WriteLine($"Price = {Price - Price * Discount}€");
+                }
+            }
+        }
+    }
+}
+            
---
+++ object-method/TaskTicket/TaskTicket/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TaskTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            ClassTicket ticket = new ClassTicket();
+            ticket.AskData();
+            ticket.ShowPrice(ticket);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
