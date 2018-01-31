712286495681c57416b6cac68377e1099f068e81
TuomasVille <tuomas_kayhty@hotmail.com>
2018-01-31 12:30:58 +0200

    TaskBookWriter 1.0

---
+++ object-method/TaskBookWriter/TaskBookWriter/ClassWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskBookWriter
+{
+    class ClassWriter
+    {
+        //Fields
+        public string Name;
+        public string Birthday;
+        ClassBook Book;
+
+        //Constructor
+        public ClassWriter(string name, string birthday, ClassBook book)
+        {
+            Name = name;
+            Birthday = birthday;
+            Book = book;
+        }
+
+        //Methods
+        public void PrintWriterInfo()
+        {
+            Console.WriteLine($"Writer name: {Name}\n" +
+                              $"Birthday: {Birthday}");
+            Book.PrintBookInfo();
+        }
+    }
+}
