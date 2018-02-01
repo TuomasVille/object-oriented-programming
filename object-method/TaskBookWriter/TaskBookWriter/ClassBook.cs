dd38ac480a21f8bc55bd90aa7a83bbab53b713b8
TuomasVille <tuomas_kayhty@hotmail.com>
2018-02-01 09:16:19 +0200

    TaskBookWriter 2.0

--- object-method/TaskBookWriter/TaskBookWriter/ClassBook.cs
+++ object-method/TaskBookWriter/TaskBookWriter/ClassBook.cs
@@ -2,93 +2,96 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace TaskBookWriter
+namespace TaskLiterature
 {
-    class ClassBook
+    namespace TaskBookWriter
     {
-        //Fields
-        public string Name;
-        public readonly string Writer;
-        public string Publisher;
-        public static string ThemeName;
-        private double _price;
-
-        //Constructor
-        public ClassBook()
+        class ClassBook
         {
-            Name = "Unknown";
-            Writer = "Unknown";
-            Publisher = "Unknown";
-            _price = 0;
-            ThemeName = "Unknown";
-        }
+            //Fields
+            public string Name;
+            public readonly string Writer;
+            public string Publisher;
+            public static string ThemeName;
+            private double _price;
 
-        public ClassBook(string name, string writer, string publisher, double price, string themename)
-        {
-            Name = name;
-            Writer = writer;
-            Publisher = publisher;
-            NewPrice = price;
-            ThemeName = themename;
-        }
-
-        //Methods
-        public void GetBook(string name)
-        {
-            if (Name.Equals(name))
+            //Constructor
+            public ClassBook()
             {
-                Console.WriteLine($"Name: {Name}\n" +
-                                  $"Writer: {Writer}\n" +
+                Name = "Unknown";
+                Writer = "Unknown";
+                Publisher = "Unknown";
+                _price = 0;
+                ThemeName = "Unknown";
+            }
+
+            public ClassBook(string name, string writer, string publisher, double price, string themename)
+            {
+                Name = name;
+                Writer = writer;
+                Publisher = publisher;
+                NewPrice = price;
+                ThemeName = themename;
+            }
+
+            //Methods
+            public void GetBook(string name)
+            {
+                if (Name.Equals(name))
+                {
+                    Console.WriteLine($"Name: {Name}\n" +
+                                      $"Writer: {Writer}\n" +
+                                      $"Publisher: {Publisher}\n" +
+                                      $"Price: {_price:C}\n" +
+                                      $"Theme: {ThemeName}");
+                }
+                else
+                {
+                    Console.WriteLine($"The book {Name} wasn´t found.");
+                }
+            }
+
+            public static void ChangeTheme(string Theme)
+            {
+                ThemeName = Theme;
+            }
+
+            public double NewPrice
+            {
+                get
+                {
+                    return _price;
+                }
+
+                set
+                {
+                    if (value > 30)
+                    {
+                        _price = value - (value * 0.1);
+                    }
+                    else
+                    {
+                        _price = value;
+                    }
+                }
+            }
+
+            public string WriterValue
+            {
+                get
+                {
+                    return Writer;
+                }
+            }
+
+            public void PrintBookInfo()
+            {
+                Console.WriteLine($"Book name: {Name}\n" +
                                   $"Publisher: {Publisher}\n" +
                                   $"Price: {_price:C}\n" +
                                   $"Theme: {ThemeName}");
             }
-            else
-            {
-                Console.WriteLine($"The book {Name} wasn´t found.");
-            }
-        }
 
-        public static void ChangeTheme(string Theme)
-        {
-            ThemeName = Theme;
         }
-
-        public double NewPrice
-        {
-            get
-            {
-                return _price;
-            }
-
-            set
-            {
-                if (value > 30)
-                {
-                    _price = value - (value * 0.1);
-                }
-                else
-                {
-                    _price = value;
-                }
-            }
-        }
-
-        public string WriterValue
-        {
-            get
-            {
-                return Writer;
-            }
-        }
-
-        public void PrintBookInfo()
-        {
-            Console.WriteLine($"Book name: {Name}\n" +
-                              $"Publisher: {Publisher}\n" +
-                              $"Price: {_price:C}\n" +
-                              $"Theme: {ThemeName}");
-        }
-            
     }
 }
--- object-method/TaskBookWriter/TaskBookWriter/ClassWriter.cs
+++ object-method/TaskBookWriter/TaskBookWriter/ClassWriter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskLiterature.TaskBookWriter;
 
 namespace TaskBookWriter
 {
