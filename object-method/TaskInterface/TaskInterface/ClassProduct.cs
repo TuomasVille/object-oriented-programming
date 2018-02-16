6f726921a5b77dcfa18f4b557015eab671f94dda
TuomasVille <tuomas_kayhty@hotmail.com>
2018-02-16 13:08:58 +0200

    TaskInterface 1.0

---
+++ object-method/TaskInterface/TaskInterface/ClassProduct.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskInterface
+{
+    class ClassProduct : IProduct
+    {
+        //Fields
+        public string Name;
+        public double UnitPrice;
+        public int Amount;
+
+        //Constructors
+        public ClassProduct(string name, double unitprice, int amount)
+        {
+            Name = name;
+            UnitPrice = unitprice;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return ($"Name: {Name}\n" +
+                    $"Unit Price: {UnitPrice:C}\n" +
+                    $"Amount: {Amount}");
+        }
+
+        //Methods
+        public void GetProduct()
+        {
+            string NewName = Name;
+            if(Name == NewName)
+            {
+                Console.WriteLine($"Name: {Name}\n" +
+                                  $"Unit Price: {UnitPrice:C}\n" +
+                                  $"Amount: {Amount}");
+            }
+            else
+            {
+                Console.WriteLine($"{null}");
+            }
+        }
+
+        public void CountTotalPrice()
+        {
+            Console.WriteLine($"Total Price: {UnitPrice * Amount:C}");
+        }
+    }
+}
---
+++ object-method/TaskInterface/TaskInterface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            ClassProduct[] product = new ClassProduct[3];
+            product[0] = new ClassProduct("Boeing Business Jet", 500000, 1);
+            product[1] = new ClassProduct("Airbus A319CJ", 7500000, 2);
+            product[2] = new ClassProduct("Gulfstream", 325000, 6);
+
+            product[0].GetProduct();
+            product[0].CountTotalPrice();
+            Console.WriteLine();
+
+            product[1].GetProduct();
+            product[1].CountTotalPrice();
+            Console.WriteLine();
+
+            product[2].GetProduct();
+            product[2].CountTotalPrice();
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
